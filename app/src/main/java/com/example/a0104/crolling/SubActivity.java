83f733eeb9ab7a4845972205bfa6d5fcf34e9ba7
akffhaos95 <38075169+akffhaos95@users.noreply.github.com>
2018-05-17 17:53:22 +0900

    LMS페이지에서 들고오는 값을 이름까지 추가함

--- app/src/main/java/com/example/a0104/crolling/SubActivity.java
+++ app/src/main/java/com/example/a0104/crolling/SubActivity.java
@@ -3,32 +3,36 @@ package com.example.a0104.crolling;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class SubActivity extends AppCompatActivity {
 
+    String [][] timeTable;
     Button btn_prev;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Sub","onCreate 호출");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
-
-        btn_prev = (Button)findViewById(R.id.btn_prev); //검색
+        btn_prev = findViewById(R.id.btn_prev); //검색
         btn_prev.setOnClickListener(new View.OnClickListener() {
             @Override //이벤트 감지자 등록
             public void onClick(View v) {
 
-                //이전 페이지로 화면 전환
-                /*Intent intent = new Intent(SubActivity.this, MainActivity.class);
-                startActivity(intent);
-                화면이 중복되므로 아래의 코드를 사용
-                */
-
-                finish(); //현재 액티비티 종료
             }
         });
+
+        Intent intent = getIntent();
+        intent.getStringArrayListExtra("timeTable");
+        intent.getStringExtra("name");
+        intent.getStringExtra("id");
+        Log.d("Sub",intent.getStringArrayListExtra("timeTable").get(0).toString());
+        Log.d("Sub",intent.getStringExtra("name"));
+        Log.d("Sub",intent.getStringExtra("id"));
+
+        MakeTimeTable makeTimeTable = new MakeTimeTable();
+        timeTable = makeTimeTable.MakeTable(intent.getStringArrayListExtra("timeTable"));
     }
 }
