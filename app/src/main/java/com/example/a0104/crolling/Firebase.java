be9fd7b82c5e505e2c621c4b41540007ec41e534
akffhaos95 <38075169+akffhaos95@users.noreply.github.com>
2018-05-25 14:54:22 +0900

    Update Firebase.java

--- app/src/main/java/com/example/a0104/crolling/Firebase.java
+++ app/src/main/java/com/example/a0104/crolling/Firebase.java
@@ -10,22 +10,22 @@ public class Firebase{
     DatabaseReference groupRef = rootRef.child("Group");
     DatabaseReference userRef = rootRef.child("User");
 
-    void updateUser(String id, String name, String table){
+    void updateUser(String id, String name, String table){ // 로그인 후에 firebase에 연결하여 user테이블에 id,name,table이 입력된다.
         Log.d("test","updateUser 호출");
         DatabaseReference childRef = userRef.child(id);
         childRef.child("Name").setValue(name);
         childRef.child("Table").setValue(table);
     }
 
-    String masterGroup(String id){
-        String Key;
+    String masterGroup(String id){ // 조장이 만드는 그룹, 랜덤으로 만들어진 Key값을 만들어내며, 다른 조원이 들어올 수 있도록한다.
+        String Key;     // 과목명을 Group/(Key)/groupName속에 넣을 수 있도록 한다.
         Log.d("test","updateGroup 호출");
         Key = groupRef.push().getKey();
         groupRef.child(Key).setValue(id);
         return Key;
     }
 
-    void slaveGroup(String Key, String id){
+    void slaveGroup(String Key, String id){ // 조원이 조장이 보내준 키값을 통해 들어올 수 있도록한다.
         Log.d("test", "enterGroup 호출");
         groupRef.child(Key).setValue(id);
     }
