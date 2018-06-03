2c581111260ab58bd3900637876c88463a33ec47
akffhaos95 <38075169+akffhaos95@users.noreply.github.com>
2018-06-03 23:20:29 +0900

    Loading.xml 구성, 해야할 것: GroupActivity 에서 GroupTable 로 인텐트 할때 groupName,key putExtra 하고, 전송할 것

--- app/src/main/java/com/example/a0104/crolling/LoadingActivity.java
+++ app/src/main/java/com/example/a0104/crolling/LoadingActivity.java
@@ -2,6 +2,7 @@ package com.example.a0104.crolling;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -18,27 +19,31 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        SharedPreferences prefs =getSharedPreferences("User", MODE_PRIVATE);
-        String name = prefs.getString("Name", null); //키값, 디폴트값
-        String id = prefs.getString("ID", null);
-        String table = prefs.getString("tableMask", null);
+        startLoading();
+    }
 
-        try { // 잠시 대기하며 로딩페이지를 보여준다.
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        if(name == null) { //데이터가 들어있는지 확인
-            Intent intent1 = new Intent(LoadingActivity.this, MainActivity.class);
-            startActivity(intent1);
-        } else {
-            Intent intent2 = new Intent(LoadingActivity.this, SubActivity.class);
-            intent2.putExtra("id",id);
-            intent2.putExtra("name",name);
-            intent2.putExtra("table",table);
-            startActivity(intent2);
-        }
-        finish();
+    private void startLoading(){
+        Handler handler =new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences prefs =getSharedPreferences("User", MODE_PRIVATE);
+                String name = prefs.getString("Name", null); //키값, 디폴트값
+                String id = prefs.getString("ID", null);
+                String table = prefs.getString("tableMask", null);
+                if(name == null) { //데이터가 들어있는지 확인
+                    Intent intent1 = new Intent(LoadingActivity.this, MainActivity.class);
+                    startActivity(intent1);
+                    finish();
+                } else {
+                    Intent intent2 = new Intent(LoadingActivity.this, SubActivity.class);
+                    intent2.putExtra("id",id);
+                    intent2.putExtra("name",name);
+                    intent2.putExtra("table",table);
+                    startActivity(intent2);
+                    finish();
+                }
+            }
+        }, 0500);
     }
 }
