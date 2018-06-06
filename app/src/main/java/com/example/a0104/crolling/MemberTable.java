1ddc91703a9a1893a92732dac1e90a111c78d392
akffhaos95 <38075169+akffhaos95@users.noreply.github.com>
2018-06-06 18:21:25 +0900

    시간표 합치기 구현완료 레이아웃 수정 필요. 그룹 삭제 기능 필요
    로그인 다시 하였을때 오류 수정필요

--- app/src/main/java/com/example/a0104/crolling/MemberTable.java
+++ app/src/main/java/com/example/a0104/crolling/MemberTable.java
@@ -3,6 +3,7 @@ package com.example.a0104.crolling;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.lang.reflect.Array;
@@ -25,22 +26,32 @@ public class MemberTable extends AppCompatActivity {
         Intent intent = getIntent();
         checkedTime = intent.getStringArrayListExtra("checkedTime");
 
-        SubActivity subActivity = null;
         for (int i=0; i < checkedTime.size(); i++){
-            member = subActivity.GroupTime(checkedTime.get(i));
+            member = GroupTime(checkedTime.get(i));
             MONDAY_0(member.get(0));//timeTable.get(0)
             tuesday_0(member.get(1));
             wednesday_0(member.get(2));
             thursday_0(member.get(3));
             friday_0(member.get(4));
+            member.clear();
         }
+    }
+
+    public ArrayList<String> GroupTime(String mask) { // mask 테이블을 요일별로 5개의 ArrayList 로 나눈다
+        ArrayList<String> table = new ArrayList<>();
+        table.add(mask.substring(0, 20));
+        table.add(mask.substring(20, 40));
+        table.add(mask.substring(40, 60));
+        table.add(mask.substring(60, 80));
+        table.add(mask.substring(80, 100));
+        return table;
     }
 
     void MONDAY_0(String mon){
         for(int i = 0; i < 20; i++){
             int getid = getResources().getIdentifier("monday" + i, "id", "com.example.a0104.crolling");
             final TextView textView = findViewById(getid);
-            if(mon.charAt(i) == 1) {
+            if(mon.charAt(i) == '1') {
                 textView.setBackgroundColor(getColor(R.color.Table_row_1));
             }
         }
@@ -49,7 +60,7 @@ public class MemberTable extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             int getid = getResources().getIdentifier("tuesday" + i, "id", "com.example.a0104.crolling");
             final TextView textView = findViewById(getid);
-            if(tue.charAt(i) == 1) {
+            if(tue.charAt(i) == '1') {
                 textView.setBackgroundColor(getColor(R.color.Table_row_2));
             }
         }
@@ -58,7 +69,7 @@ public class MemberTable extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             int getid = getResources().getIdentifier("wednesday" + i, "id", "com.example.a0104.crolling");
             final TextView textView = findViewById(getid);
-            if(wed.charAt(i) == 1){
+            if(wed.charAt(i) == '1'){
                 textView.setBackgroundColor(getColor(R.color.Table_row_3));
             }
         }
@@ -67,7 +78,7 @@ public class MemberTable extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             int getid = getResources().getIdentifier("thursday" + i, "id", "com.example.a0104.crolling");
             final TextView textView = findViewById(getid);
-            if(tue.charAt(i) == 1) {
+            if(tue.charAt(i) == '1') {
                 textView.setBackgroundColor(getColor(R.color.Table_row_4));
             }
         }
@@ -76,7 +87,7 @@ public class MemberTable extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             int getid = getResources().getIdentifier("friday" + i, "id", "com.example.a0104.crolling");
             final TextView textView = findViewById(getid);
-            if (fri.charAt(i) == 1) {
+            if (fri.charAt(i) == '1') {
                 textView.setBackgroundColor(getColor(R.color.Table_row_5));
             }
         }
